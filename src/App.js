6e3b9ec85785378b81b8a31b004a25d6842f74eb
ThomasGodard <thomasgodard2@gmail.com>
2019-05-08 16:25:41 +0200

    add Core UI free template

--- src/App.js
+++ src/App.js
@@ -1,79 +1,36 @@
-import React, { Component } from "react";
-import "./App.css";
-import { BrowserRouter, Redirect, Route } from "react-router-dom";
-import Login from "./views/login/Login";
-import Signup from "./views/signup/Signup";
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+// import { renderRoutes } from 'react-router-config';
+import './App.scss';
+
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+
+// Containers
+const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/Pages/Login'));
+const Register = React.lazy(() => import('./views/Pages/Register'));
+const Page404 = React.lazy(() => import('./views/Pages/Page404'));
+const Page500 = React.lazy(() => import('./views/Pages/Page500'));
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-      isAuthenticated: isAuth
-    };
-  }
-
-  handleLogin = (data) => {
-    console.log("onLogin: ", data);
-    this.setState({user: data});
-    isAuth = true;
-  };
 
   render() {
     return (
-      <BrowserRouter>
-        <Route
-          path="/login"
-          render={ props => (
-           <Login
-             {...props}
-              onLogin={this.handleLogin}
-           />
-          )}
-        />
-        <Route
-          path="/signup"
-          component={Signup}
-        />
-
-        {this.state.isAuthenticated && <h1>MENU</h1>}
-        <PrivateRoute exact path="/"
-                      component={ props => (
-                        <Test
-                          {...props}
-                          user={this.state.user}
-                        />
-                      )}
-        />
-      </BrowserRouter>
+      <HashRouter>
+          <React.Suspense fallback={loading()}>
+            <Switch>
+              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
+              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
+              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
+              <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
+            </Switch>
+          </React.Suspense>
+      </HashRouter>
     );
   }
-}
-
-let isAuth = false;
-
-function PrivateRoute({ component: Component, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        isAuth ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{
-              pathname: "/login",
-              state: { from: props.location }
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-function Test({user}) {
-  return (<h2>Test ! {user.email + " " + user.userName}</h2>);
 }
 
 export default App;
